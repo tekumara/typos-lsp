42b36d1cd55cd00b882babacf8c915eefd37f2b9
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-04-08 23:09:27 +1000

    feat: better error handling + loglevel

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import {
     commands,
     ConfigurationChangeEvent,
     ExtensionContext,
-	OutputChannel,
+    OutputChannel,
 } from "vscode";
 
 import {
@@ -14,23 +14,21 @@ import {
     Executable,
 } from "vscode-languageclient/node";
 
-let client: LanguageClient;
+let client: LanguageClient | undefined;
 
-
-export function activate(context: ExtensionContext)  {
-	let name = "Typos";
+export function activate(context: ExtensionContext) {
+    let name = "Typos";
 
     const outputChannel = window.createOutputChannel(name);
     context.subscriptions.push(outputChannel);
 
-    client = createClient(name, outputChannel);
-
     context.subscriptions.push(
         workspace.onDidChangeConfiguration(
             async (e: ConfigurationChangeEvent) => {
-                const restartTriggeredBy = ["typos.path"].find((s) =>
-                    e.affectsConfiguration(s)
-                );
+                const restartTriggeredBy = [
+                    "typos.path",
+                    "typos.logLevel",
+                ].find((s) => e.affectsConfiguration(s));
 
                 if (restartTriggeredBy) {
                     await commands.executeCommand("typos.restart");
@@ -41,27 +39,34 @@ export function activate(context: ExtensionContext)  {
 
     context.subscriptions.push(
         commands.registerCommand("typos.restart", async () => {
-            //void window.showInformationMessage("Restarting typos...");
+            // can't stop if the client has previously failed to start
+            if (client && client.needsStop()) {
+                await client.stop();
+            }
 
-			// don't stop if the client has previously failed to start
-			if (client.needsStop()) {
-            	await client.stop();
-			}
-
-			client = createClient(name, outputChannel);
+            try {
+                client = createClient(name, outputChannel);
+            } catch (err) {
+                window.showErrorMessage(
+                    `Typos: ${err instanceof Error ? err.message : err}`
+                );
+                return;
+            }
             await client.start();
         })
     );
+
+    client = createClient(name, outputChannel);
 
     // Start the client. This will also launch the server
     client.start();
 }
 
-function createClient(name: string, outputChannel: OutputChannel): LanguageClient {
+function createClient(
+    name: string,
+    outputChannel: OutputChannel
+): LanguageClient {
     const env = { ...process.env };
-
-    // TODO: move into config
-    env.RUST_LOG = "trace";
 
     let config = workspace.getConfiguration("typos");
     let path = config.get<null | string>("path");
@@ -69,6 +74,8 @@ function createClient(name: string, outputChannel: OutputChannel): LanguageClien
     if (!path) {
         throw new Error(`Please specify the typos.path setting.`);
     }
+
+    env.RUST_LOG = config.get("logLevel");
 
     const run: Executable = {
         command: path,
@@ -86,16 +93,11 @@ function createClient(name: string, outputChannel: OutputChannel): LanguageClien
         // We use scheme = file to ignore Untitled documents because that generates
         // too much request chatter
         documentSelector: [{ scheme: "file", pattern: "**" }],
-		outputChannel: outputChannel,
-		traceOutputChannel: outputChannel,
+        outputChannel: outputChannel,
+        traceOutputChannel: outputChannel,
     };
 
-    return new LanguageClient(
-        "typos",
-        name,
-        serverOptions,
-        clientOptions
-    );
+    return new LanguageClient("typos", name, serverOptions, clientOptions);
 }
 
 export function deactivate(): Thenable<void> | undefined {

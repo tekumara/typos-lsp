a54e82812f427eb60c203260d98d83474214d096
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-04-09 15:56:58 +1000

    add --lsp cli arg

---
+++ crates/typos-cli/src/bin/typos-cli/args.rs
@@ -0,0 +1,303 @@
+use clap::builder::TypedValueParser;
+use clap::Parser;
+
+use typos_cli::config;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, clap::ValueEnum)]
+pub enum Format {
+    Silent,
+    Brief,
+    Long,
+    Json,
+}
+
+impl Format {
+    pub(crate) fn reporter(
+        self,
+        stdout_palette: crate::report::Palette,
+        stderr_palette: crate::report::Palette,
+    ) -> Box<dyn typos_cli::report::Report> {
+        match self {
+            Format::Silent => Box::new(crate::report::PrintSilent),
+            Format::Brief => Box::new(crate::report::PrintBrief {
+                stdout_palette,
+                stderr_palette,
+            }),
+            Format::Long => Box::new(crate::report::PrintLong {
+                stdout_palette,
+                stderr_palette,
+            }),
+            Format::Json => Box::new(crate::report::PrintJson),
+        }
+    }
+}
+
+impl Default for Format {
+    fn default() -> Self {
+        Format::Long
+    }
+}
+
+#[derive(Debug, Parser)]
+#[command(rename_all = "kebab-case")]
+#[command(about, author, version)]
+#[command(
+        color = concolor_clap::color_choice(),
+    )]
+#[command(group = clap::ArgGroup::new("mode").multiple(false))]
+pub(crate) struct Args {
+    /// Paths to check with `-` for stdin
+    #[arg(default_value = ".")]
+    pub(crate) path: Vec<std::path::PathBuf>,
+
+    /// Custom config file
+    #[arg(short = 'c', long = "config")]
+    pub(crate) custom_config: Option<std::path::PathBuf>,
+
+    /// Ignore implicit configuration files.
+    #[arg(long)]
+    pub(crate) isolated: bool,
+
+    /// Print a diff of what would change
+    #[arg(long, group = "mode")]
+    pub(crate) diff: bool,
+
+    /// Write fixes out
+    #[arg(long, short = 'w', group = "mode")]
+    pub(crate) write_changes: bool,
+
+    /// Debug: Print each file that would be spellchecked.
+    #[arg(long, group = "mode")]
+    pub(crate) files: bool,
+
+    /// Debug: Print each file's type
+    #[arg(long, group = "mode")]
+    pub(crate) file_types: bool,
+
+    /// Debug: Print each identifier that would be spellchecked.
+    #[arg(long, group = "mode")]
+    pub(crate) identifiers: bool,
+
+    /// Debug: Print each word that would be spellchecked.
+    #[arg(long, group = "mode")]
+    pub(crate) words: bool,
+
+    /// Write the current configuration to file with `-` for stdout
+    #[arg(long, group = "mode")]
+    pub(crate) dump_config: Option<std::path::PathBuf>,
+
+    /// Show all supported file types.
+    #[arg(long, group = "mode")]
+    pub(crate) type_list: bool,
+
+    /// Start the LSP server.
+    #[arg(long, group = "mode")]
+    pub(crate) lsp: bool,
+
+    #[arg(long, value_enum, ignore_case = true, default_value("long"))]
+    pub(crate) format: Format,
+
+    /// The approximate number of threads to use.
+    #[arg(short = 'j', long = "threads", default_value = "0")]
+    pub(crate) threads: usize,
+
+    #[command(flatten)]
+    pub(crate) config: ConfigArgs,
+
+    #[command(flatten)]
+    pub(crate) color: concolor_clap::Color,
+
+    #[command(flatten)]
+    pub(crate) verbose: clap_verbosity_flag::Verbosity,
+}
+
+#[derive(Debug, Clone, clap::Args)]
+#[command(rename_all = "kebab-case")]
+pub(crate) struct FileArgs {
+    /// Search binary files.
+    #[arg(long, overrides_with("no_binary"))]
+    binary: bool,
+    #[arg(long, overrides_with("binary"), hide = true)]
+    no_binary: bool,
+
+    /// Skip verifying spelling in file names.
+    #[arg(long, overrides_with("check_filenames"))]
+    no_check_filenames: bool,
+    #[arg(long, overrides_with("no_check_filenames"), hide = true)]
+    check_filenames: bool,
+
+    /// Skip verifying spelling in files.
+    #[arg(long, overrides_with("check_files"))]
+    no_check_files: bool,
+    #[arg(long, overrides_with("no_check_files"), hide = true)]
+    check_files: bool,
+
+    #[arg(long, overrides_with("no_unicode"), hide = true)]
+    unicode: bool,
+    /// Only allow ASCII characters in identifiers
+    #[arg(long, overrides_with("unicode"))]
+    no_unicode: bool,
+
+    #[arg(long)]
+    #[arg(
+        value_parser = clap::builder::PossibleValuesParser::new(config::Locale::variants())
+            .map(|l| l.parse::<config::Locale>().unwrap())
+    )]
+    pub(crate) locale: Option<config::Locale>,
+}
+
+impl FileArgs {
+    pub fn to_config(&self) -> config::EngineConfig {
+        config::EngineConfig {
+            binary: self.binary(),
+            check_filename: self.check_filename(),
+            check_file: self.check_file(),
+            tokenizer: Some(config::TokenizerConfig {
+                unicode: self.unicode(),
+                ..Default::default()
+            }),
+            dict: Some(config::DictConfig {
+                locale: self.locale,
+                ..Default::default()
+            }),
+            extend_ignore_re: Default::default(),
+        }
+    }
+
+    fn binary(&self) -> Option<bool> {
+        resolve_bool_arg(self.binary, self.no_binary)
+    }
+
+    fn check_filename(&self) -> Option<bool> {
+        resolve_bool_arg(self.check_filenames, self.no_check_filenames)
+    }
+
+    fn unicode(&self) -> Option<bool> {
+        resolve_bool_arg(self.unicode, self.no_unicode)
+    }
+
+    fn check_file(&self) -> Option<bool> {
+        resolve_bool_arg(self.check_files, self.no_check_files)
+    }
+}
+
+#[derive(Debug, clap::Args)]
+#[command(rename_all = "kebab-case")]
+pub(crate) struct ConfigArgs {
+    #[command(flatten)]
+    walk: WalkArgs,
+    #[command(flatten)]
+    overrides: FileArgs,
+}
+
+impl ConfigArgs {
+    pub fn to_config(&self) -> config::Config {
+        config::Config {
+            files: self.walk.to_config(),
+            overrides: self.overrides.to_config(),
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Debug, clap::Args)]
+#[command(rename_all = "kebab-case")]
+pub(crate) struct WalkArgs {
+    /// Ignore files & directories matching the glob.
+    #[arg(long, value_name = "GLOB")]
+    exclude: Vec<String>,
+
+    /// Search hidden files and directories.
+    #[arg(long, overrides_with("no_hidden"))]
+    hidden: bool,
+    #[arg(long, overrides_with("hidden"), hide = true)]
+    no_hidden: bool,
+
+    /// Don't respect ignore files.
+    #[arg(long, overrides_with("ignore"))]
+    no_ignore: bool,
+    #[arg(long, overrides_with("no_ignore"), hide = true)]
+    ignore: bool,
+
+    /// Don't respect .ignore files.
+    #[arg(long, overrides_with("ignore_dot"))]
+    no_ignore_dot: bool,
+    #[arg(long, overrides_with("no_ignore_dot"), hide = true)]
+    ignore_dot: bool,
+
+    /// Don't respect global ignore files.
+    #[arg(long, overrides_with("ignore_global"))]
+    no_ignore_global: bool,
+    #[arg(long, overrides_with("no_ignore_global"), hide = true)]
+    ignore_global: bool,
+
+    /// Don't respect ignore files in parent directories.
+    #[arg(long, overrides_with("ignore_parent"))]
+    no_ignore_parent: bool,
+    #[arg(long, overrides_with("no_ignore_parent"), hide = true)]
+    ignore_parent: bool,
+
+    /// Don't respect ignore files in vcs directories.
+    #[arg(long, overrides_with("ignore_vcs"))]
+    no_ignore_vcs: bool,
+    #[arg(long, overrides_with("no_ignore_vcs"), hide = true)]
+    ignore_vcs: bool,
+}
+
+impl WalkArgs {
+    pub fn to_config(&self) -> config::Walk {
+        config::Walk {
+            extend_exclude: self.exclude.clone(),
+            ignore_hidden: self.ignore_hidden(),
+            ignore_files: self.ignore_files(),
+            ignore_dot: self.ignore_dot(),
+            ignore_vcs: self.ignore_vcs(),
+            ignore_global: self.ignore_global(),
+            ignore_parent: self.ignore_parent(),
+        }
+    }
+
+    fn ignore_hidden(&self) -> Option<bool> {
+        resolve_bool_arg(self.no_hidden, self.hidden)
+    }
+
+    fn ignore_files(&self) -> Option<bool> {
+        resolve_bool_arg(self.ignore, self.no_ignore)
+    }
+
+    fn ignore_dot(&self) -> Option<bool> {
+        resolve_bool_arg(self.ignore_dot, self.no_ignore_dot)
+    }
+
+    fn ignore_vcs(&self) -> Option<bool> {
+        resolve_bool_arg(self.ignore_vcs, self.no_ignore_vcs)
+    }
+
+    fn ignore_global(&self) -> Option<bool> {
+        resolve_bool_arg(self.ignore_global, self.no_ignore_global)
+    }
+
+    fn ignore_parent(&self) -> Option<bool> {
+        resolve_bool_arg(self.ignore_parent, self.no_ignore_parent)
+    }
+}
+
+fn resolve_bool_arg(yes: bool, no: bool) -> Option<bool> {
+    match (yes, no) {
+        (true, false) => Some(true),
+        (false, true) => Some(false),
+        (false, false) => None,
+        (_, _) => unreachable!("clap should make this impossible"),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn verify_app() {
+        use clap::CommandFactory;
+        Args::command().debug_assert()
+    }
+}
---
+++ crates/typos-cli/src/bin/typos-cli/main.rs
@@ -0,0 +1,319 @@
+use std::io::Write;
+
+use clap::Parser;
+
+mod args;
+mod report;
+
+use proc_exit::prelude::*;
+
+fn main() {
+    human_panic::setup_panic!();
+    let result = run();
+    proc_exit::exit(result);
+}
+
+fn run() -> proc_exit::ExitResult {
+    // clap's `get_matches` uses Failure rather than Usage, so bypass it for `get_matches_safe`.
+    let args = match args::Args::try_parse() {
+        Ok(args) => args,
+        Err(e) if e.use_stderr() => {
+            let _ = e.print();
+            return proc_exit::sysexits::USAGE_ERR.ok();
+        }
+        Err(e) => {
+            let _ = e.print();
+            return proc_exit::Code::SUCCESS.ok();
+        }
+    };
+
+    args.color.apply();
+
+    init_logging(args.verbose.log_level());
+
+    if let Some(output_path) = args.dump_config.as_ref() {
+        run_dump_config(&args, output_path)
+    } else if args.type_list {
+        run_type_list(&args)
+    } else if args.lsp {
+        run_lsp(&args)
+    } else {
+        run_checks(&args)
+    }
+}
+
+fn run_dump_config(args: &args::Args, output_path: &std::path::Path) -> proc_exit::ExitResult {
+    let global_cwd = std::env::current_dir().to_sysexits()?;
+
+    let path = &args.path[0];
+    let cwd = if path == std::path::Path::new("-") {
+        global_cwd
+    } else if path.is_file() {
+        let mut cwd = path
+            .canonicalize()
+            .with_code(proc_exit::sysexits::USAGE_ERR)?;
+        cwd.pop();
+        cwd
+    } else {
+        path.canonicalize()
+            .with_code(proc_exit::sysexits::USAGE_ERR)?
+    };
+    let cwd = cwd
+        .canonicalize()
+        .with_code(proc_exit::sysexits::USAGE_ERR)?;
+
+    let storage = typos_cli::policy::ConfigStorage::new();
+    let mut engine = typos_cli::policy::ConfigEngine::new(&storage);
+    engine.set_isolated(args.isolated);
+
+    let mut overrides = typos_cli::config::Config::default();
+    if let Some(path) = args.custom_config.as_ref() {
+        let custom = typos_cli::config::Config::from_file(path)
+            .with_code(proc_exit::sysexits::CONFIG_ERR)?;
+        overrides.update(&custom);
+    }
+    overrides.update(&args.config.to_config());
+    engine.set_overrides(overrides);
+
+    let config = engine
+        .load_config(&cwd)
+        .with_code(proc_exit::sysexits::CONFIG_ERR)?;
+
+    let mut defaulted_config = typos_cli::config::Config::from_defaults();
+    defaulted_config.update(&config);
+    let output = toml::to_string_pretty(&defaulted_config).with_code(proc_exit::Code::FAILURE)?;
+    if output_path == std::path::Path::new("-") {
+        std::io::stdout()
+            .write_all(output.as_bytes())
+            .to_sysexits()?;
+    } else {
+        std::fs::write(output_path, &output).to_sysexits()?;
+    }
+
+    Ok(())
+}
+
+fn run_type_list(args: &args::Args) -> proc_exit::ExitResult {
+    let global_cwd = std::env::current_dir().to_sysexits()?;
+
+    let path = &args.path[0];
+    let cwd = if path == std::path::Path::new("-") {
+        global_cwd
+    } else if path.is_file() {
+        let mut cwd = path
+            .canonicalize()
+            .with_code(proc_exit::sysexits::USAGE_ERR)?;
+        cwd.pop();
+        cwd
+    } else {
+        path.canonicalize()
+            .with_code(proc_exit::sysexits::USAGE_ERR)?
+    };
+    let cwd = cwd
+        .canonicalize()
+        .with_code(proc_exit::sysexits::USAGE_ERR)?;
+
+    let storage = typos_cli::policy::ConfigStorage::new();
+    let mut engine = typos_cli::policy::ConfigEngine::new(&storage);
+    engine.set_isolated(args.isolated);
+
+    let mut overrides = typos_cli::config::Config::default();
+    if let Some(path) = args.custom_config.as_ref() {
+        let custom = typos_cli::config::Config::from_file(path)
+            .with_code(proc_exit::sysexits::CONFIG_ERR)?;
+        overrides.update(&custom);
+    }
+    overrides.update(&args.config.to_config());
+    engine.set_overrides(overrides);
+
+    engine
+        .init_dir(&cwd)
+        .with_code(proc_exit::sysexits::CONFIG_ERR)?;
+    let definitions = engine.file_types(&cwd);
+
+    let stdout = std::io::stdout();
+    let mut handle = stdout.lock();
+    for (name, globs) in definitions {
+        writeln!(handle, "{}: {}", name, itertools::join(globs, ", ")).to_sysexits()?;
+    }
+
+    Ok(())
+}
+
+fn run_lsp(_: &args::Args) -> proc_exit::ExitResult {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .with_writer(std::io::stderr)
+        .init();
+
+    let runtime = tokio::runtime::Runtime::new().with_code(proc_exit::Code::FAILURE)?;
+    runtime.block_on(async {
+        let stdin = tokio::io::stdin();
+        let stdout = tokio::io::stdout();
+
+        let (service, socket) = tower_lsp::LspService::new(typos_cli::lsp::Backend::new);
+        tower_lsp::Server::new(stdin, stdout, socket).serve(service).await;
+    });
+    proc_exit::Code::SUCCESS.ok()
+}
+
+fn run_checks(args: &args::Args) -> proc_exit::ExitResult {
+    let global_cwd = std::env::current_dir().to_sysexits()?;
+
+    let storage = typos_cli::policy::ConfigStorage::new();
+    let mut engine = typos_cli::policy::ConfigEngine::new(&storage);
+    engine.set_isolated(args.isolated);
+
+    let mut overrides = typos_cli::config::Config::default();
+    if let Some(path) = args.custom_config.as_ref() {
+        let custom = typos_cli::config::Config::from_file(path)
+            .with_code(proc_exit::sysexits::CONFIG_ERR)?;
+        overrides.update(&custom);
+    }
+    overrides.update(&args.config.to_config());
+    engine.set_overrides(overrides);
+
+    let mut typos_found = false;
+    let mut errors_found = false;
+    for path in args.path.iter() {
+        let cwd = if path == std::path::Path::new("-") {
+            global_cwd.clone()
+        } else if path.is_file() {
+            let mut cwd = path
+                .canonicalize()
+                .with_code(proc_exit::sysexits::USAGE_ERR)?;
+            cwd.pop();
+            cwd
+        } else {
+            path.canonicalize()
+                .with_code(proc_exit::sysexits::USAGE_ERR)?
+        };
+
+        engine
+            .init_dir(&cwd)
+            .with_code(proc_exit::sysexits::CONFIG_ERR)?;
+        let walk_policy = engine.walk(&cwd);
+
+        let threads = if path.is_file() { 1 } else { args.threads };
+        let single_threaded = threads == 1;
+
+        let mut walk = ignore::WalkBuilder::new(path);
+        walk.threads(args.threads)
+            .skip_stdout(true)
+            .hidden(walk_policy.ignore_hidden())
+            .ignore(walk_policy.ignore_dot())
+            .git_global(walk_policy.ignore_global())
+            .git_ignore(walk_policy.ignore_vcs())
+            .git_exclude(walk_policy.ignore_vcs())
+            .parents(walk_policy.ignore_parent());
+        if !walk_policy.extend_exclude.is_empty() {
+            let mut overrides = ignore::overrides::OverrideBuilder::new(".");
+            for pattern in walk_policy.extend_exclude.iter() {
+                overrides
+                    .add(&format!("!{}", pattern))
+                    .with_code(proc_exit::sysexits::CONFIG_ERR)?;
+            }
+            let overrides = overrides
+                .build()
+                .with_code(proc_exit::sysexits::CONFIG_ERR)?;
+            walk.overrides(overrides);
+        }
+
+        // HACK: Diff doesn't handle mixing content
+        let output_reporter = if args.diff {
+            Box::new(crate::report::PrintSilent)
+        } else {
+            let stdout_palette = report::Palette::colored();
+            let stderr_palette = report::Palette::colored();
+            args.format.reporter(stdout_palette, stderr_palette)
+        };
+        let status_reporter = report::MessageStatus::new(output_reporter.as_ref());
+        let reporter: &dyn typos_cli::report::Report = &status_reporter;
+
+        let selected_checks: &dyn typos_cli::file::FileChecker = if args.files {
+            &typos_cli::file::FoundFiles
+        } else if args.file_types {
+            &typos_cli::file::FileTypes
+        } else if args.identifiers {
+            &typos_cli::file::Identifiers
+        } else if args.words {
+            &typos_cli::file::Words
+        } else if args.write_changes {
+            &typos_cli::file::FixTypos
+        } else if args.diff {
+            &typos_cli::file::DiffTypos
+        } else {
+            &typos_cli::file::Typos
+        };
+
+        if single_threaded {
+            typos_cli::file::walk_path(walk.build(), selected_checks, &engine, reporter)
+        } else {
+            typos_cli::file::walk_path_parallel(
+                walk.build_parallel(),
+                selected_checks,
+                &engine,
+                reporter,
+            )
+        }
+        .map_err(|e| {
+            e.io_error()
+                .map(|i| {
+                    let kind = i.kind();
+                    proc_exit::sysexits::io_to_sysexists(kind)
+                        .or_else(|| proc_exit::bash::io_to_signal(kind))
+                        .unwrap_or(proc_exit::sysexits::IO_ERR)
+                })
+                .unwrap_or_default()
+                .with_message(e)
+        })?;
+        if status_reporter.typos_found() {
+            typos_found = true;
+        }
+        if status_reporter.errors_found() {
+            errors_found = true;
+        }
+    }
+
+    if errors_found {
+        proc_exit::Code::FAILURE.ok()
+    } else if typos_found {
+        // Can;'t use `Failure` since its so prevalent, it could be easy to get a
+        // `Failure` from something else and get it mixed up with typos.
+        //
+        // Can't use DataErr or anything else an std::io::ErrorKind might map to.
+        proc_exit::Code::new(2).ok()
+    } else {
+        proc_exit::Code::SUCCESS.ok()
+    }
+}
+
+fn init_logging(level: Option<log::Level>) {
+    if let Some(level) = level {
+        let mut builder = env_logger::Builder::new();
+
+        let colored = concolor::get(concolor::Stream::Stderr).ansi_color();
+        builder.write_style(if colored {
+            env_logger::WriteStyle::Always
+        } else {
+            env_logger::WriteStyle::Never
+        });
+
+        builder.filter(None, level.to_level_filter());
+
+        if level == log::LevelFilter::Trace {
+            builder.format_timestamp_secs();
+        } else {
+            builder.format(|f, record| {
+                writeln!(
+                    f,
+                    "[{}] {}",
+                    record.level().to_string().to_lowercase(),
+                    record.args()
+                )
+            });
+        }
+
+        builder.init();
+    }
+}

3727999f1afcbd2f7104af2707d197c29ee2d2c4
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-04-09 11:31:29 +1000

    build: add diagnostics test

---
+++ src/test/suite/diagnostics.test.ts
@@ -0,0 +1,34 @@
+import * as vscode from 'vscode';
+import * as assert from 'assert';
+import { getDocUri, activate } from './helper';
+
+suite('Should get diagnostics', () => {
+	const docUri = getDocUri('diagnostics.txt');
+
+	test('Diagnoses typo', async () => {
+		await testDiagnostics(docUri, [
+			{ message: '`fo` should be `of`, `for`', range: toRange(1, 5, 1, 7), severity: vscode.DiagnosticSeverity.Warning, source: 'ex' },
+		]);
+	});
+});
+
+function toRange(sLine: number, sChar: number, eLine: number, eChar: number) {
+	const start = new vscode.Position(sLine, sChar);
+	const end = new vscode.Position(eLine, eChar);
+	return new vscode.Range(start, end);
+}
+
+async function testDiagnostics(docUri: vscode.Uri, expectedDiagnostics: vscode.Diagnostic[]) {
+	await activate(docUri);
+
+	const actualDiagnostics = vscode.languages.getDiagnostics(docUri);
+
+	assert.equal(actualDiagnostics.length, expectedDiagnostics.length);
+
+	expectedDiagnostics.forEach((expectedDiagnostic, i) => {
+		const actualDiagnostic = actualDiagnostics[i];
+		assert.equal(actualDiagnostic.message, expectedDiagnostic.message);
+		assert.deepEqual(actualDiagnostic.range, expectedDiagnostic.range);
+		assert.equal(actualDiagnostic.severity, expectedDiagnostic.severity);
+	});
+}
---
+++ src/test/suite/helper.ts
@@ -0,0 +1,33 @@
+import * as vscode from "vscode";
+import * as path from "path";
+
+export let doc: vscode.TextDocument;
+export let editor: vscode.TextEditor;
+
+/**
+ * Activates the extension
+ */
+export async function activate(docUri: vscode.Uri) {
+    const ext = vscode.extensions.getExtension("tekumara.typos-vscode")!;
+	// TODO: configure path
+	await ext.activate();
+    try {
+        doc = await vscode.workspace.openTextDocument(docUri);
+        editor = await vscode.window.showTextDocument(doc);
+        await sleep(2000); // Wait for server activation
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+async function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export const getDocPath = (p: string) => {
+    return path.resolve(__dirname, "../../../src/test/fixture", p);
+};
+
+export const getDocUri = (p: string) => {
+    return vscode.Uri.file(getDocPath(p));
+};

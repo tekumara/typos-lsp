70204aa8497f80205498326f036f4e82a1abbd8b
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-04-09 13:10:49 +1000

    refactor: move typos-lsp into typos-lci

---
+++ crates/typos-cli/src/lib.rs
@@ -0,0 +1,20 @@
+/// `typos_cli`'s API is unstable.  Open an issue for starting a discussion on getting a subset
+/// stabilized.
+
+#[doc(hidden)]
+pub mod config;
+#[doc(hidden)]
+pub mod dict;
+#[doc(hidden)]
+pub mod file;
+#[doc(hidden)]
+pub mod lsp;
+#[doc(hidden)]
+pub mod check;
+#[doc(hidden)]
+pub mod policy;
+#[doc(hidden)]
+pub mod report;
+
+mod default_types;
+mod file_type;

d1b1e1427f0168a0cdb52b0588c9fc80872c719d
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2026-04-03 14:57:46 +1100

    fix mocha

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,5 +1,5 @@
 import * as path from "path";
-import * as Mocha from "mocha";
+import Mocha = require("mocha");
 import { glob } from "glob";
 
 export function run(): Promise<void> {
@@ -19,7 +19,7 @@ export function run(): Promise<void> {
 
       try {
         // Run the mocha test
-        mocha.run((failures) => {
+        mocha.run((failures: number) => {
           if (failures > 0) {
             e(new Error(`${failures} tests failed.`));
           } else {
